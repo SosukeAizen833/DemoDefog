c6250eaaa6623184562c53c78d729c90b90acc9c
SosukeAizen833 <Spandan833@gmai.com>
2022-03-02 23:24:59 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const display = () =>{
+    const image = document.createElement('img');
+    image.src ="ExwEzM4XEAg6ptW_0.png";
+    document.body.append(image);
+    
+}
+document.getElementById('display-btn').addEventListener('click',() =>{
+    display();
+    
+})
